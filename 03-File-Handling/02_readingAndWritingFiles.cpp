4fdb1b8a2ad037dccd5dc538eae6044823cea7b9
sakkkky <sontakkesaksham1510@gmail.com>
2026-06-14 23:54:41 +0530

    intial commit

---
+++ 03-File-Handling/02_readingAndWritingFiles.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream> //->> we have to include 'fstream'
+using namespace std; 
+
+// File I/O in C++: Reading and Writing Files in C++
+/*
+  # These are some useful classes for working with files in C++
+  1. fstreambase
+  2. ifstream --> derived from fstreambase (uses in() --> 'in' means retriveing data from this file   )
+  3. ofstream --> derived from fstreambase (uses out() --> 'out' mean setting data inside a file)
+
+  # In order to work with files in C++, you will have to open it. Primarily, there are 2 ways to open a file:
+  1. Using the constructor
+  2. Using the member function open() of the class
+
+*/
+
+int main(){ 
+
+    string para1 = "Hello World";
+    string para2;
+
+    //writing data
+    ofstream out("Files\02_test_1.txt");
+    out<<para1; //--> it uses cout syntax
+
+    //getting data
+    ifstream in("Files\02_test_2.txt");
+    in>>para2; //--> it uses cin syntaax
+    /*
+        1.the above code will only read data for first line to get another line we have to 
+        use  getline(in, para2); 
+        2. each time we run it we move to diffrent lines 
+    */
+
+    cout<<para2;
+    getline(in , para2);
+    cout<<para2;
+    getline(in , para2);
+    cout<<para2;
+    getline(in , para2);
+    cout<<para2;
+    getline(in , para2);
+    cout<<para2;
+    
+
+    return 0;
+}
